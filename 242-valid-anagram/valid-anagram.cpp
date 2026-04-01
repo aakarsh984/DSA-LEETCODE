528e54b0654a59b728d9b45f1368d87099ebe0fc
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-01 17:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (91.36%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-      map<char, int> m1,m2;
+        int freq[26] = {0};
 if(s.size()!=t.size()) return false;
-      for(int i=0;i<s.size();i++){
-            m1[s[i]]++;
-            m2[t[i]]++;
-      }
-    if(m1==m2){
-        return true;
-    }
-    return false;
+        for (int i = 0; i < s.size(); i++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
 
-        
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0)
+                return false;
+        }
+        return true;
     }
 };
