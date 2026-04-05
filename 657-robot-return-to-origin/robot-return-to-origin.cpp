3ada192b34c26205dfc9810676bc63e5e060ef0f
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-05 13:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (95.43%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     bool judgeCircle(string moves) {
         //r=1 u=1 l=-1 d=-1
-        int r=count(moves.begin(),moves.end(),'R');
-        int l=count(moves.begin(),moves.end(),'L');
-        int u=count(moves.begin(),moves.end(),'U');
-        int d=count(moves.begin(),moves.end(),'D');
+       int x=0,y=0;
+       for(int i=0;i<moves.length();i++){
+     
+        if(moves[i]=='R')x++;
+           if(moves[i]=='L')x--;
+           if(moves[i]=='U')y++;
+           if(moves[i]=='D')y--;
+       }
 
-        return (r==l)&&(u==d);
+        return (x==0)&&(y==0);
     }
 };
