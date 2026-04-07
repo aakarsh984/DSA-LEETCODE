f6687962d64326fb92f4e97ed91e845e0b18265d
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-07 09:26:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (75.00%) - LeetSync

--- 3188-find-champion-i/find-champion-i.cpp
+++ 3188-find-champion-i/find-champion-i.cpp
@@ -4,7 +4,9 @@ public:
         int win=0;
         for(int i=0;i<grid.size();i++){
             if(win==i)continue;
-            if(grid[i][win]==1) win=i;
+            if(grid[win]<grid[i]){
+                win=i;
+            }
         }
         return win;
         
