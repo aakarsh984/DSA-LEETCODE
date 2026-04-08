e5978d6fbfbd780275bc0283a8226cfa500bd068
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-08 09:29:43 +0530

    Time: 82 ms (34.45%) | Memory: 158 MB (91.87%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
@@ -4,13 +4,13 @@ public:
         int MOD = 1e9 + 7;
         for(int i=0;i<queries.size();i++){
             int idx= queries[i][0];
-            int ri=queries[i][1];
-            int v=queries[i][3];
-            int k=queries[i][2];
+            // int ri=queries[i][1];
+            // int v=queries[i][3];
+            // int k=queries[i][2];
 
-        while(idx<=ri){
-            nums[idx]=((long long)nums[idx]*v)% MOD;
-            idx+=k;
+        while(idx<=queries[i][1]){
+            nums[idx]=((long long)nums[idx]*queries[i][3])% MOD;
+            idx+=queries[i][2];
         }
         }
         int x = 0;
