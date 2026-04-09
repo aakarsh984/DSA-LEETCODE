10e6eac975810bb8f1c510de475a4d2e5cf5ca9f
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-09 09:18:33 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (97.73%) - LeetSync

--- 520-detect-capital/detect-capital.cpp
+++ 520-detect-capital/detect-capital.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int c=0,l=0;
-        if(word.length()==1 && islower(word[0])) return true;
+        int c=0;
+        // if(word.length()==1 && islower(word[0])) return true;
         for(int i=0;i<word.length();i++){
             if(isupper(word[i])){
                 c++;
             }
-            if(islower(word[i])){
-                l++;
-            }
         }
-        if(word.length()==c||(isupper(word[0])&&c==1) ||l==word.size() ){
+        if(word.length()==c||(isupper(word[0])&&c==1) ||c==0 ){
             return true;
         }
         return false;
