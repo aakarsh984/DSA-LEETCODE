27407ddc48914ed291ef24478efc1bc7f4093ab3
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-09 10:23:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (22.63%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_map<char,int>mp;
+        for(auto x:s){
+            mp[x]++;
+            if(mp[x]==2){
+                return x;
+            }
+        }
+
+       return 'A'; 
+    }
+};
