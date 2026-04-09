921dff87ad0db809adc6a6c36d3ac5cce45085a6
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-09 09:57:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (79.92%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.cpp
+++ 551-student-attendance-record-i/student-attendance-record-i.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     bool checkRecord(string s) {
-        int total =s.length();
-        int a=0;
-        for(int i = 0; i < s.length(); i++){
-            if(s[i]=='A')a++;
-        }
-        if(s.find("LLL")==string::npos && a<2 )return true;
-        return false;
+        int a = 0, l = 0;
 
-        
+        for (char c : s) {
+            if (c == 'A') {
+                a++;
+                if (a >= 2) return false;
+                l = 0;
+            }
+            else if (c == 'L') {
+                l++;
+                if (l >= 3) return false;
+            }
+            else {
+                l = 0;
+            }
+        }
+        return true;
     }
 };
