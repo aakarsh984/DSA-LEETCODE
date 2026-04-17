803db7f94813c6403f2bdcba978f98c3b83a0b10
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-17 12:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (51.24%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,41 +1,55 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int r=matrix.size();
-        int c= matrix[0].size();
-        int fr=0;
-        int lr=r-1;
-        int fc=0;
-        int lc=c-1;
-        vector<int>ans;
+        int r = matrix.size();
+        int c = matrix[0].size();
+
+        int fr = 0, lr = r - 1;
+        int fc = 0, lc = c - 1;
+
+        vector<int> ans;
 
         while (fr <= lr && fc <= lc) {
 
+            int i = fr, j = fc;
+
             // left → right
-            for (int j = fc; j <= lc; j++)
+            j = fc;
+            while (j <= lc) {
                 ans.push_back(matrix[fr][j]);
+                j++;
+            }
             fr++;
 
             // top → bottom
-            for (int i = fr; i <= lr; i++)
+            i = fr;
+            while (i <= lr) {
                 ans.push_back(matrix[i][lc]);
+                i++;
+            }
             lc--;
 
             // right → left
             if (fr <= lr) {
-                for (int j = lc; j >= fc; j--)
+                j = lc;
+                while (j >= fc) {
                     ans.push_back(matrix[lr][j]);
+                    j--;
+                }
                 lr--;
             }
 
             // bottom → top
             if (fc <= lc) {
-                for (int i = lr; i >= fr; i--)
+                i = lr;
+                while (i >= fr) {
                     ans.push_back(matrix[i][fc]);
+                    i--;
+                }
                 fc++;
             }
         }
+
         return ans;
-        
     }
 };
