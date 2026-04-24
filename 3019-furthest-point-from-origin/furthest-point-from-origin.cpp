7de6c09fe5dd1b0c26f4be9b649dde28ed872906
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-24 12:30:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (30.54%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        int x=0,d=0;
-        int c=0;
-        for(int i=0;i<moves.size();i++){
-            if(moves[i]=='R')x++;
-            if(moves[i]=='L')x--;
-            if(moves[i]=='_')c++;
+        int l=0,r=0,d=0;
+        for(int i = 0; i < moves.size(); i++){
+            if(moves[i]=='L')l++;
+            else if(moves[i]=='R')r++;
+            else d++;
         }
-        if(x>=0)return x+c;
-        return abs(x-c);
+        if(l>r){
+            return l+d-r;
+        }
+        else{
+            return r+d-l;
+        }
+
+        
     }
 };
