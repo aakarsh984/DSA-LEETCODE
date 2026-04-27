e4599817139ddd28992d18d058234117c98eabfa
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-27 17:22:40 +0530

    Time: 0 ms (100.00%) | Memory: 31.6 MB (37.50%) - LeetSync

--- 4290-valid-elements-in-an-array/valid-elements-in-an-array.cpp
+++ 4290-valid-elements-in-an-array/valid-elements-in-an-array.cpp
@@ -3,25 +3,25 @@ public:
     vector<int> findValidElements(vector<int>& nums) {
         int n = nums.size();
 
-        vector<bool> left_valid(n, false);
+        vector<bool> valid(n, false);
         int maxSoFar = INT_MIN;
         for (int i = 0; i < n; i++) {
             if (nums[i] > maxSoFar)
-                left_valid[i] = true;
+                valid[i] = true;
             maxSoFar = max(maxSoFar, nums[i]);
         }
 
-        vector<bool> right_valid(n, false);
+        // vector<bool> right_valid(n, false);
         maxSoFar = INT_MIN;
         for (int i = n - 1; i >= 0; i--) {
             if (nums[i] > maxSoFar)
-                right_valid[i] = true;
+                valid[i] = true;
             maxSoFar = max(maxSoFar, nums[i]);
         }
 
         vector<int> result;
         for (int i = 0; i < n; i++)
-            if (left_valid[i] || right_valid[i])
+            if (valid[i])
                 result.push_back(nums[i]);
 
         return result;
