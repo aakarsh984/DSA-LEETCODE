67cc043a8da5dd3b89ebee2e2527841e397b40fa
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-04-28 18:12:08 +0530

    Time: 29 ms (76.13%) | Memory: 90.2 MB (34.57%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,31 +1,35 @@
-static const int _ = []() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    return 0;
-}();
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int>v;
-        for(int i=0;i<grid.size();i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                v.push_back(grid[i][j]);
-            }
-        }
-       int n=v.size();
-       sort(v.begin(),v.end());
-       int target =v[n/2];
-    int ans=0;
-        for(int i=0;i<n;i++){
-            if(v[i]==target) continue;
-            if(abs((target-v[i]))%x==0 ){
-                ans+=abs(target-v[i])/x;
-            }
-            else {
-                return -1;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> vec;
+        //O(m*n)
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                vec.push_back(grid[i][j]);
             }
         }
 
-        return ans;
+        int L = vec.size(); //m*n
+
+        //sort(begin(vec), end(vec)); //O(LlogL)
+        nth_element(begin(vec), begin(vec) + L/2, end(vec)); //O(L)
+        //{4, 2, 8, 6}
+        //{4, 2, 6, 8} - nth_element
+        //{2, 4, 6, 8} - complete sorting
+
+        int target = vec[L/2];
+        int result = 0;
+        for(int &num : vec) { //O(m*n)
+            if(num%x != target%x) {
+                return -1;
+            }
+
+            result += abs(target-num)/x;
+        }
+
+        return result;
     }
 };
