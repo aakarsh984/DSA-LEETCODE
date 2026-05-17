a3d9c8130bd9c4561468f7e447dafdb201e9e733
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-05-17 10:31:50 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (48.35%) - LeetSync

--- 4305-check-adjacent-digit-differences/check-adjacent-digit-differences.cpp
+++ 4305-check-adjacent-digit-differences/check-adjacent-digit-differences.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool isAdjacentDiffAtMostTwo(string s) {
-        int n=s.length();
-        // if(s.length()==2) return false;
-        int diff=0;
+   
+     
 
-        for(int i = 0; i < n-1; i++){
-            diff=abs((s[i] - '0')-(s[i+1] - '0'));
-            if(diff>=3) return false;
+        for(int i = 0; i < s.length()-1; i++){
+           
+            if(abs((s[i] - '0')-(s[i+1] - '0'))>=3) return false;
         }
          return true;
 
