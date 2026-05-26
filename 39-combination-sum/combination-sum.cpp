12704ba81d80b678c45a03e30512f1e1113a5520
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-05-26 11:41:36 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (36.22%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
+
     vector<vector<int>> ans;
-    vector<int>sub;
+    vector<int> sub;
     int n;
-    void solve(vector<int>& candidates, int target,int i,int subsum){
-         if(subsum == target) {
+
+    void solve(vector<int>& candidates, int target, int i, int subsum) {
+
+        if(subsum == target) {
             ans.push_back(sub);
             return;
         }
@@ -12,21 +15,24 @@ public:
         if(i >= n || subsum > target) {
             return;
         }
-        subsum+=candidates[i];
-        sub.push_back(candidates[i]);
-        //stay at curernt element
-        solve(candidates,target,i,subsum);
 
-        //go to next element 
-        subsum-=candidates[i];
+        // pick
+        sub.push_back(candidates[i]);
+
+        solve(candidates, target, i, subsum + candidates[i]);
+
         sub.pop_back();
-        solve(candidates,target,i+1,subsum);
-        
+
+        // not pick
+        solve(candidates, target, i + 1, subsum);
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        n=candidates.size();
-        solve(candidates,target,0,0);
-         return ans;
-        
+
+        n = candidates.size();
+
+        solve(candidates, target, 0, 0);
+
+        return ans;
     }
 };
