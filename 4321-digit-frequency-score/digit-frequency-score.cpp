ff78722699ce89b8af6166146a49d3670460dbca
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-05-31 11:41:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (83.23%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.cpp
+++ 4321-digit-frequency-score/digit-frequency-score.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int digitFrequencyScore(int n) {
         vector<int>freq(10,0);
-        int temp=n;
-        while(temp>0){
-            freq[temp%10]++;
-            temp/=10;
+       
+        while(n>0){
+            freq[n%10]++;
+            n/=10;
         }
         int ans=0;
         for(int i = 0; i < 10; i++){
