be58fa7688d5028c46fdfd2aebdc76267b0d4912
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-01 22:27:02 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.35%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,31 +2,30 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n=nums.size();
-        int l=1;
-        int r=n-2;
-        if(n==1) return nums[0];
-        if(nums[0]!=nums[1]) return nums[0];
-        if(nums[n-1]!=nums[n-2]) return nums[n-1];
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }
-            if(mid%2 ==1 && nums[mid]!=nums[mid-1]){
-                r=mid-1;
-            }
-             else if(mid%2 ==0 && nums[mid]!=nums[mid+1]){
-               r = mid - 1;
-             }
-             else  if(mid%2 ==1 && nums[mid]==nums[mid-1]){
-                l=mid+1;
-            }
-            else if(mid%2 ==0 && nums[mid]==nums[mid+1]){
-                  l = mid+1;  
-            }
+        int l=0;
+        int h=n-1;
 
+        while(l<h){
+            int mid=l+(h-l)%2;
+            bool iseven=(h-mid)/2;
+            if(nums[mid]==nums[mid+1]){
+                if(iseven){
+                  l=mid+2;
+                }
+                else{
+                   h=mid-1;
+                }
+            }
+            else{
+                if(iseven){
+                  h=mid;
+                }
+                else{
+                   l=mid+1;
+                }
+            }
         }
-         return nums[l];
+        return nums[h];
         
     }
 };
