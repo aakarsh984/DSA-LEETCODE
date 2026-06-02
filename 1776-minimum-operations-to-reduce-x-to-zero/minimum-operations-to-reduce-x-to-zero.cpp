c0d76b7a379f1c3c42f204aef0ca703fb14e8c7c
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-02 10:09:45 +0530

    Time: 2 ms (73.87%) | Memory: 102.5 MB (40.10%) - LeetSync

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int n=nums.size();
+        int total=accumulate(nums.begin(),nums.end(),0);
+        if(total<x)return -1;
+        int rem=total-x;
+        int i=0,j=0;
+        int ans=-1;
+        int sum=0;
+        while(j<n){
+             sum+=nums[j];
+            while(sum>rem && i<=j){
+                sum-=nums[i];
+                i++;
+            }
+            if(sum==rem){
+                ans = max(ans, j - i + 1);
+            }
+           
+            j++;
+        }
+        return ans==-1?-1:n-ans;
+        
+    }
+};
