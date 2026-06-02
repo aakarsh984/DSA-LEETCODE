b24f01b7affd1e283d3cde0d6469ff1737f95223
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-02 17:56:57 +0530

    Time: 3 ms (89.95%) | Memory: 40.1 MB (4.54%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-    int ans=0;
-        for(int i = 0; i < nums.size(); i=i+2){
-            ans+=nums[i];
+        vector<int> count(20001, 0);
+        for (auto x : nums) {
+            count[x + 10000]++;
         }
 
-         return ans;
-        
+        int maxSum = 0;
+        bool flag = true;
+
+        for (int i = 0; i < count.size(); i++) {
+            if (count[i] > 0) {
+                if (flag)
+                    maxSum += i - 10000;
+
+                flag = !flag;
+                count[i]--;
+                i--;
+            }
+        }
+        return maxSum;
     }
 };
