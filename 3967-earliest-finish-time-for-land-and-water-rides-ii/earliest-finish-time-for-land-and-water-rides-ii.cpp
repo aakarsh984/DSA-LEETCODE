e2dd2b100003ef992ace018ca2c4f26548de17da
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-03 09:09:34 +0530

    Time: 4 ms (73.39%) | Memory: 237.9 MB (92.66%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int n=landStartTime.size();
-        int m=waterStartTime.size();
+    int earliestFinishTime(vector<int>& landStartTime,
+                           vector<int>& landDuration,
+                           vector<int>& waterStartTime,
+                           vector<int>& waterDuration) {
+        int n = landStartTime.size();
+        int m = waterStartTime.size();
 
-        int land=INT_MAX;
-        int water=INT_MAX;
+        int land = INT_MAX;
+    
 
-        int wtl=INT_MAX;
-        int ltw=INT_MAX;
+        // int land = INT_MAX;
+        for (int i = 0; i < n; i++)
+            land = min(land, landStartTime[i] + landDuration[i]);
 
-        for(int i = 0; i < n; i++){
-            land=min(land,landStartTime[i]+landDuration[i]);
-        }
-        for(int i = 0; i < m; i++){
-            ltw=min(ltw,max(land,waterStartTime[i])+waterDuration[i]);
-            water=min(water,waterStartTime[i]+waterDuration[i]);
-        }
-        for(int i = 0; i < n; i++){
-            wtl=min(wtl,max(water,landStartTime[i])+landDuration[i]);
-        }
-        return min(ltw,wtl);
-        
+        int water = INT_MAX;
+        for (int i = 0; i < m; i++)
+            water = min(water, waterStartTime[i] + waterDuration[i]);
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i < m; i++)
+            ans = min(ans, max(land, waterStartTime[i]) + waterDuration[i]);
+
+        for (int i = 0; i < n; i++)
+            ans = min(ans, max(water, landStartTime[i]) + landDuration[i]);
+
+        return ans;
     }
 };
