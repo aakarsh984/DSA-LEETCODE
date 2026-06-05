5b12151ec34509095d6df38c6d507dfa8abe82a8
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-05 16:24:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (5.45%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -5,9 +5,9 @@ public:
         int s=0;
         int e=n-1;
         while(s<e){
-            int mid=s+(e-s)/2;
-                if(nums[mid]>nums[mid+1]) e=mid;
-                else s=mid+1;
+            int mid=s+(e-s+1)/2;
+                if(nums[mid]>nums[mid-1]) s=mid;
+                else e=mid-1;
         }
          
         return s;
