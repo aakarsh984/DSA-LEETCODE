3296e7203279f29f1e9d3867e240c37187ae4cf6
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-08 23:37:50 +0530

    Time: 931 ms (5.16%) | Memory: 331.7 MB (5.02%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool ispossible(int n, vector<int> q,int mid){
+        int i=0;int j=0;
+        vector<int>prod(n,0);
+      while(i<q.size()){
+
+        if(q[i]>=mid){
+            prod[j++]=mid;
+            q[i]-=mid;
+        }
+        else{
+            prod[j++]=q[i];
+            q[i]=0;
+        }
+        if(j==n) break;
+        if(q[i]==0){
+            i++;
+        }
+
+      }
+        for(int i = 0; i < q.size(); i++){
+            if(q[i]!=0)return false;
+        }
+         return true;
+
+    }
+    int minimizedMaximum(int n, vector<int>& q) {
+        // int m=q.size();
+        int low = 1;
+        int high = *max_element(q.begin(), q.end());
+        int ans = INT_MIN;
+         while (low<=high){
+            int mid=low+(high-low)/2;
+            if(ispossible(n,q,mid)){
+                high=mid-1;
+               ans=mid;
+            }
+            else{
+                low= mid+1;
+            }
+
+         }
+         return ans;
+    }
+};
