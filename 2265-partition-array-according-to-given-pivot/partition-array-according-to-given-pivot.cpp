3311fe6da56b9d6f161cf897e73e9befa1898ea4
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-09 01:19:08 +0530

    Time: 6 ms (66.03%) | Memory: 127.6 MB (87.87%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n=nums.size();
-        int countless=0;
-        int counteq=0;
-        for(int i = 0; i < n; i++){
-            if(nums[i]<pivot){
-                countless++;
-            }
-            else if(nums[i]==pivot){
-                counteq++;
-            }
-        }
-        int k=counteq+countless;
-        int i=0;
-        int j=countless;
+        int n = nums.size();
 
-        vector<int>ans(n);
-        for(int x: nums){
-            if(x<pivot){
-                ans[i++]=x;
-            }
-            else if(x==pivot){
-                ans[j++]=x;
-            }
-            else{
-                ans[k++]=x;
-            }
-        }
-return ans;
+        vector<int> result(n);
 
-        
+        //original nums
+        int i = 0;
+        int j = n-1;
+
+        //result variables
+        int ires = 0;
+        int jres = n-1;
+
+        while(i < n && j >= 0) {
+            if(nums[i] < pivot) {
+                result[ires] = nums[i];
+                ires++;
+            }
+
+            if(nums[j] > pivot) {
+                result[jres] = nums[j];
+                jres--;
+            }
+
+            i++;
+            j--;
+        }
+
+        //ires to jres -> fill the pivot
+        while(ires <= jres) {
+            result[ires] = pivot;
+            ires++;
+        }
+
+        return result;
+
     }
 };
