f9586c7eb4f3fbc0b62859a8d085e3d5351bc786
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-09 17:12:33 +0530

    Time: 34 ms (80.27%) | Memory: 106 MB (32.53%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
-//bs on numbers from idx to mid and find the operation needed to convert
-    int bsearch(int idx,vector<int>& nums, int k,vector<long long>&pref){
-        int l=0;
-        int r=idx;
-      
-        int res=0;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            long long count=idx-mid+1;
-            long long total=count*nums[idx];
-            long long curr=pref[idx]-pref[mid]+nums[mid];
-        
-            if(total-curr>k){
-                l=mid+1;
-            }
-            else{
-                res=mid;
-                r=mid-1;
-            }
-        }
-
-        return idx-res+1;
-    }
-
     int maxFrequency(vector<int>& nums, int k) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        vector<long long>pref(n,0);
-        pref[0]=nums[0];
-        for(int i=1;i<n;i++){
-            pref[i]=pref[i-1]+nums[i];
-        }
-        int res=INT_MIN;
-        for(int i=0;i<n;i++){
-            res=max(bsearch(i,nums,k,pref),res);
-        }
+        long sum=0;
+        int i=0;
+        int j=0;
+        int ans=INT_MIN;
+        while(j<n){
+            sum+=nums[j];
+            long total=1LL*nums[j]*(j-i+1);
+            long ops=total-sum;
+                while(ops>k){
+                    sum-=nums[i++];
+                    total=1LL*nums[j]*(j-i+1);
+                    ops=total-sum;
+                }
+                    ans=max(ans,j-i+1);
+                    j++;
+                
 
-        return res;
+        } return ans;
         
     }
 };
