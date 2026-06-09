8414a0e91bbe602c37c6f618abe2d6774d818f0e
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-09 11:07:40 +0530

    Time: 25 ms (10.90%) | Memory: 17.5 MB (11.89%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -7,7 +7,7 @@ public:
 
         int i = 0;
         int r = 0;
-        int c = 0;
+
         bool down = true;
         while (i < s.size()) {
             if (down) { // down will go vertically straight
@@ -22,7 +22,6 @@ public:
             } else { // going up diagonaly c+1 row-1
                 if (r >= 0) {
                     v[r--].push_back(s[i]);
-                    c++;
                     i++;
                 } else {
                     down = true;
