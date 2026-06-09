eb3be593adc936cc20939570fa9b26ea3f2fc923
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-09 10:57:51 +0530

    Time: 17 ms (18.14%) | Memory: 17.6 MB (11.89%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+
+        vector<vector<char>> v(numRows);
+        if(numRows==1)return s;
+
+        int i = 0;
+        int r = 0;
+        int c = 0;
+        bool down = true;
+        while (i < s.size()) {
+            if (down) { // down will go vertically straight
+                if (r < numRows) {
+                    v[r++].push_back(s[i]);
+                    i++;
+                } else {
+                    down = false;
+                    r = numRows - 2;
+                }
+
+            } else { // going up diagonaly c+1 row-1
+                if (r >= 0) {
+                    v[r--].push_back(s[i]);
+                    c++;
+                    i++;
+                } else {
+                    down = true;
+                    r = 1;
+                }
+            }
+        }
+        string ans;
+
+        for (int i = 0; i < numRows; i++) {
+            for (char ch : v[i]) {
+                ans += ch;
+            }
+        }
+
+        return ans;
+    }
+};
