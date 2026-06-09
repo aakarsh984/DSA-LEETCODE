8c361bf69ef53c370e5bb1a84012c92b13e2e6f2
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-09 16:50:17 +0530

    Time: 117 ms (5.05%) | Memory: 115 MB (10.95%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -3,18 +3,15 @@ public:
     int bsearch(int idx,vector<int>& nums, int k,vector<long long>&pref){
         int l=0;
         int r=idx;
-        long long ogsum=0;
+      
         int res=0;
         while(l<=r){
             int mid=l+(r-l)/2;
             long long count=idx-mid+1;
             long long total=count*nums[idx];
-            if(mid==0)
-             ogsum=pref[idx];
-            else  ogsum=pref[idx]-pref[mid-1];
-           
-
-            if(total-ogsum>k){
+            long long curr=pref[idx]-pref[mid]+nums[mid];
+        
+            if(total-curr>k){
                 l=mid+1;
             }
             else{
