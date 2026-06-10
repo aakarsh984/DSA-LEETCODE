d21873b97684c2a2dfcbda1385a5df6a1c916b01
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-10 15:06:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (21.52%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -7,27 +7,34 @@ public:
         vector<int>suff(n,0);
 
         pref[0]=boxes[0]-'0';
+        int psum=pref[0];
         for(int i = 1; i < n; i++){
             pref[i]=pref[i-1]+(boxes[i]-'0');
+            ans[i]=psum;
+            psum+=pref[i];
+
         }
         suff[n-1]=boxes[n-1]-'0';
+         int ssum=suff[n-1];
         for(int i=n-2;i>=0;i--){
             suff[i]=suff[i+1]+(boxes[i]-'0');
+            ans[i]+=ssum;
+            ssum+=suff[i];
         }
 
-        for(int i = 0; i < n; i++){
-            //pref from 0 to i-1;
-            int psum=0;
-            int ssum=0;
-            for(int j = 0; j <i ; j++){
-                psum+=pref[j];
-            }
-            for(int j = i+1; j <n ; j++){
-                ssum+=suff[j];
-            }
-            ans[i]=psum+ssum;
+        // for(int i = 0; i < n; i++){
+        //     //pref from 0 to i-1;
+        //     int psum=0;
+        //     int ssum=0;
+        //     for(int j = 0; j <i ; j++){
+        //         psum+=pref[j];
+        //     }
+        //     for(int j = i+1; j <n ; j++){
+        //         ssum+=suff[j];
+        //     }
+        //     ans[i]=psum+ssum;
 
-        }
+        // }
     return ans;
     }
     
