174e2d273950ccd4a75bcc255081826577ca1f78
Aakarsh Gupta <aakarshgupta067@gmail.com>
2026-06-12 12:01:03 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (79.09%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        //  vector<int> ans;
-        if (nums.size() == 2)
+    
+         if (nums.size() == 2)
             return nums;
         int xor1 = 0;
         for (int x : nums) {
             xor1 ^= x;
         }
+       
+        //in XOR find the first set bit  that is the diff bit and then make group according to them 
         int pos = 0;
         while ((xor1 & 1) == 0) {
             xor1 >>= 1;
